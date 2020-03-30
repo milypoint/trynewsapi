027ce59387e5d1a87a2047abe109bc30033901b0
Andrii Tokar <milypointo@gmail.com>
2020-03-30 20:56:13 +0300

    app\newsapi\Config: fixed exeption handle. hopefully

--- app/newsapi/Config.php
+++ app/newsapi/Config.php
@@ -5,6 +5,7 @@ namespace app\newsapi;
 class Config
 {
 	private static $_file = ROOT .'/'. __NAMESPACE__ . '/config.json';
+	private static $_default_token = 'Your news api token here.';
 
 	public static function token()
 	{
@@ -13,10 +14,10 @@ class Config
 			$content = file_get_contents(self::$_file);
 			$content = json_decode($content);
 			try {
-				if (isset($content->token)) {
+				if (isset($content->token) and $content->token !== self::$_default_token) {
 					return $content->token;
 				} else {
-					throw new \Exception('Token not found in' . self::$_file);
+					throw new \Exception('Token not found in ' . self::$_file);
 				}
 			} catch (\Exception $e) {
 				echo $e->getMessage();
@@ -25,12 +26,22 @@ class Config
 
 		} else {
 			// create config file
-			$handle = fopen(self::$_file, 'w') or die('Cannot create file:  ' . self::$_file);
-			$data = ['token' => 'Your news api token here.'];
-			$data = json_encode($data);
-			fwrite($handle, $data);
-			fclose($handle);
-			echo 'Put toke into  ' . self::$_file;
+			try {
+				$handle = fopen(self::$_file, 'w');
+				if (!$handle) {
+					throw new \Exception('Cant create file '.self::$_file);
+				} else {
+					$data = ['token' => self::$_default_token];
+					$data = json_encode($data);
+					fwrite($handle, $data);
+					fclose($handle);
+				}
+			} catch (\Exception $e) {
+				echo $e->getMessage();
+			}
+
+
+			echo 'Put token into  ' . self::$_file;
 			return null;
 		}
 	}
